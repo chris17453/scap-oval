01e7652fe665e8aaaec950dad0c2b9a9c65f3e53
Watkins, Charles <charles.watkins@etrade.com>
2018-07-24 15:03:08 -0400

    Note namespace removal

--- oval/_derived_class/NotesType/notes.cs
+++ oval/_derived_class/NotesType/notes.cs
@@ -3,8 +3,8 @@ using System.Xml;
 using System.Xml.Serialization; 
  namespace oval{       [SerializableAttribute]
     [XmlTypeAttribute(AnonymousType=true, Namespace="http://oval.mitre.org/XMLSchema/oval-definitions-5")]
-    [XmlRootAttribute(Namespace="http://oval.mitre.org/XMLSchema/oval-definitions-5", IsNullable=false)]
-    public class notes : NotesType {
+    [XmlRootAttribute(ElementName="notes",Namespace="http://oval.mitre.org/XMLSchema/oval-definitions-5", IsNullable=false)]
+    public class notes  {
         private string[] note1Field;
         [XmlElementAttribute("note")]
         public string[] note1 {
